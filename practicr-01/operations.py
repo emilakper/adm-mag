de20c831fcfea10bcb2cf49e1504e76e31a7a8e2
Maximov-Ivan <113625972+Maximov-Ivan@users.noreply.github.com>
2024-02-14 14:59:42 +0300

    Update operations.py

--- practicr-01/operations.py
+++ practicr-01/operations.py
@@ -6,6 +6,10 @@ rdigits = {0 : '0', 1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7
            8 : '8', 9 : '9', 10 : 'A', 11 :'B', 12 : 'C', 13 : 'D', 14 : 'E', 15 : 'F'}
 
 def to_decimal(number, ss):
+    negative = False
+    if number[0] == '-':
+        negative = True
+        number = number[1:]
     pos = number.find('.') 
     power = 0
     if pos == -1:
@@ -17,6 +21,8 @@ def to_decimal(number, ss):
         if digit != '.':
             decimal += digits[digit] * (ss ** power)
             power -= 1
+    if negative:
+        decimal = -decimal
     return decimal
 
 def from_decimal(decimal, ss):
@@ -37,69 +43,28 @@ def from_decimal(decimal, ss):
         power -= 1
     if number[-1] == '.':
         number = number[:-1]
-    if number == "0":
+    if len(number) < 2:
         return number
     if number[0] == '0' and number[1] != '.':
         number = number[1:]
     if negative:
-        number = negate(number)
+        number = '-' + number
     return number
 
 def transform(number, ss_from, ss_to):
     decimal = to_decimal(number, ss_from)
     return from_decimal(decimal, ss_to)
 
-def is_neative(number):
-    if number[0] == '-':
-        return True
-    return False
-
-def negate(number):
-    if number[0] == '-':
-        number = number[1:]
-    else:
-        number = '-' + number
-    return number
-
-def modulo(number):
-    if is_neative(number):
-        return negate(number)
-    return number
-
 def plus(number1, number2, ss):
-    if is_neative(number1) and is_neative(number2):
-        return negate(from_decimal(to_decimal(negate(number1), ss) + to_decimal(negate(number2), ss), ss))
-    elif is_neative(number1):
-        return from_decimal(to_decimal(number2, ss) - to_decimal(negate(number1), ss), ss)
-    elif is_neative(number2):
-        return from_decimal(to_decimal(number1, ss) - to_decimal(negate(number2), ss), ss)
     return from_decimal(to_decimal(number1, ss) + to_decimal(number2, ss), ss)
 
 def minus(number1, number2, ss):
-    if is_neative(number1) and is_neative(number2):
-        return from_decimal(to_decimal(negate(number2), ss) - to_decimal(negate(number1), ss), ss)
-    elif is_neative(number1):
-        return negate(from_decimal(to_decimal(negate(number1), ss) + to_decimal(number2, ss), ss))
-    elif is_neative(number2):
-        return from_decimal(to_decimal(number1, ss) + to_decimal(negate(number2), ss), ss)
     return from_decimal(to_decimal(number1, ss) - to_decimal(number2, ss), ss)
 
 def multiply(number1, number2, ss):
-    if is_neative(number1) and is_neative(number2):
-        return from_decimal(to_decimal(negate(number1), ss) * to_decimal(negate(number2), ss), ss)
-    elif is_neative(number1):
-        return negate(from_decimal(to_decimal(negate(number1), ss) * to_decimal(number2, ss), ss))
-    elif is_neative(number2):
-        return negate(from_decimal(to_decimal(number1, ss) * to_decimal(negate(number2), ss), ss))
     return from_decimal(to_decimal(number1, ss) * to_decimal(number2, ss), ss)
 
 def divide(number1, number2, ss):
-    if to_decimal(modulo(number2), ss) == 0:
+    if to_decimal(number2, ss) == 0:
         return "Error"
-    if is_neative(number1) and is_neative(number2):
-        return from_decimal(to_decimal(negate(number1), ss) / to_decimal(negate(number2), ss), ss)
-    elif is_neative(number1):
-        return negate(from_decimal(to_decimal(negate(number1), ss) / to_decimal(number2, ss), ss))
-    elif is_neative(number2):
-        return negate(from_decimal(to_decimal(number1, ss) / to_decimal(negate(number2), ss), ss))
     return from_decimal(to_decimal(number1, ss) / to_decimal(number2, ss), ss)
